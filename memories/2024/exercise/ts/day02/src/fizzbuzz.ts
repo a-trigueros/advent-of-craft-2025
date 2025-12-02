7dc160a63637e10a85ad14c1344d1cd0ae79152c
Alexandre Trigueros <80602661+a-trigueros@users.noreply.github.com>
2025-12-02 22:55:00 +0100

    feat: day2 in typescript

--- memories/2024/exercise/ts/day02/src/fizzbuzz.ts
+++ memories/2024/exercise/ts/day02/src/fizzbuzz.ts
@@ -1,27 +1,36 @@
-import {none, Option, some} from "fp-ts/Option";
+import { none, Option, some } from "fp-ts/Option";
 
-export const min = 1;
-export const max = 100;
-
-let mapping: Map<number, string> = new Map([
-    [15, 'FizzBuzz'],
-    [3, 'Fizz'],
-    [5, 'Buzz'],
-]);
-
-export const fizzbuzz = (input: number): Option<string> =>
-    isOutOfRange(input)
-        ? none
-        : some(convertSafely(input));
-
-function convertSafely(input: number): string {
-    for (const [divisor, value] of mapping) {
-        if (is(divisor, input)) {
-            return value;
-        }
-    }
-    return input.toString();
+export type FizzBuzzConfig = {
+  min: number;
+  max: number;
+  textByDivisors: Map<number, string>;
 }
 
-const is = (divisor: number, input: number): boolean => input % divisor === 0;
-const isOutOfRange = (input: number): boolean => input < min || input > max;
+export function fizzBuzzSetup(config: FizzBuzzConfig) {
+  const min = config.min;
+  const max = config.max;
+
+  let mapping: Map<number, string> = config.textByDivisors;
+
+  return (input: number): Option<string> =>
+    isOutOfRange(input)
+      ? none
+      : some(convertSafely(input));
+
+  function convertSafely(input: number): string {
+    for (const [divisor, value] of mapping) {
+      if (is(divisor, input)) {
+        return value;
+      }
+    }
+    return input.toString();
+  }
+
+  function is(divisor: number, input: number): boolean {
+    return input % divisor === 0;
+  }
+
+  function isOutOfRange(input: number): boolean {
+    return input < min || input > max;
+  }
+}
--- memories/2024/exercise/ts/day02/tests/fizzbuzz.spec.ts
+++ memories/2024/exercise/ts/day02/tests/fizzbuzz.spec.ts
@@ -1,54 +1,110 @@
-import {fizzbuzz, max, min} from '../src/fizzbuzz';
+import { FizzBuzzConfig, fizzBuzzSetup } from '../src/fizzbuzz';
 import * as O from 'fp-ts/Option';
-import {isNone, isSome} from 'fp-ts/Option';
+import { isNone, isSome } from 'fp-ts/Option';
 import * as fc from 'fast-check';
-import {pipe} from 'fp-ts/function';
+import { pipe } from 'fp-ts/function';
 
-describe('FizzBuzz should return', () => {
-    test.each([
-        [1, '1'],
-        [67, '67'],
-        [82, '82'],
-        [3, 'Fizz'],
-        [66, 'Fizz'],
-        [99, 'Fizz'],
-        [5, 'Buzz'],
-        [50, 'Buzz'],
-        [85, 'Buzz'],
-        [15, 'FizzBuzz'],
-        [30, 'FizzBuzz'],
-        [45, 'FizzBuzz']
-    ])('its representation %s -> %s', (input, expectedResult) => {
-        const conversionResult = fizzbuzz(input);
-        expect(isSome(conversionResult)).toBeTruthy();
+function runTests(
+  config: FizzBuzzConfig,
+  samples: [number, string][]
+) {
 
-        if (isSome(conversionResult)) {
-            expect(conversionResult.value).toBe(expectedResult);
-        }
+  const fizzbuzz = fizzBuzzSetup(config);
+
+  describe('FizzBuzz should return', () => {
+    test.each(samples)('its representation %s -> %s', (input, expectedResult) => {
+      const conversionResult = fizzbuzz(input);
+      expect(isSome(conversionResult)).toBeTruthy();
+
+      if (isSome(conversionResult)) {
+        expect(conversionResult.value).toBe(expectedResult);
+      }
     });
 
     test('valid strings for numbers between 1 and 100', () => {
-        fc.assert(
-            fc.property(
-                fc.integer().filter(n => n >= min && n <= max),
-                (n) => isConvertValid(n)
-            )
-        );
+      fc.assert(
+        fc.property(
+          fc.integer().filter(n => n >= config.min && n <= config.max),
+          (n) => isConvertValid(n)
+        )
+      );
     });
 
     const isConvertValid = (input: number): boolean => pipe(
-        fizzbuzz(input),
-        O.exists(result => validStringsFor(input).includes(result))
+      fizzbuzz(input),
+      O.exists(result => validStringsFor(input).indexOf(result) >= 0)
     );
 
-    const validStringsFor = (x: number): string[] => ['Fizz', 'Buzz', 'FizzBuzz', x.toString()];
+    const validStringsFor = (x: number): string[] => [...config.textByDivisors.values(), x.toString()];
 
     test('none for numbers out of range', () => {
-        fc.assert(
-            fc.property(
-                fc.integer().filter(n => n < min || n > max),
-                (n) => isNone(fizzbuzz(n))
-            )
-        );
+      fc.assert(
+        fc.property(
+          fc.integer().filter(n => n < config.min || n > config.max),
+          (n) => isNone(fizzbuzz(n))
+        )
+      );
     });
-});
+  });
+}
+
+runTests({
+  min: 1,
+  max: 100,
+  textByDivisors: new Map<number, string>([
+    [7 * 11, 'WhizzBang'],
+    [5 * 11, 'BuzzBang'],
+    [3 * 11, 'FizzBang'],
+    [5 * 7, 'BuzzWhizz'],
+    [3 * 7, 'FizzWhizz'],
+    [3 * 5, 'FizzBuzz'],
+    [11, 'Bang'],
+    [7, "Whizz"],
+    [3, 'Fizz'],
+    [5, 'Buzz'],
+  ])
+}, [
+  [1, '1'],
+  [67, '67'],
+  [82, '82'],
+  [3, 'Fizz'],
+  [66, 'FizzBang'],
+  [99, 'FizzBang'],
+  [5, 'Buzz'],
+  [50, 'Buzz'],
+  [85, 'Buzz'],
+  [15, 'FizzBuzz'],
+  [30, 'FizzBuzz'],
+  [45, 'FizzBuzz'],
+  [7, 'Whizz'],
+  [28, 'Whizz'],
+  [77, 'WhizzBang'],
+  [11, 'Bang'],
+  [22, 'Bang'],
+  [33, 'FizzBang'],
+  [44, 'Bang'],
+  [55, 'BuzzBang'],
+]);
+
+runTests({
+  min: 1,
+  max: 100,
+  textByDivisors: new Map<number, string>([
+    [15, 'FizzBuzz'],
+    [3, 'Fizz'],
+    [5, 'Buzz'],
+  ])
+}, [
+  [1, '1'],
+  [67, '67'],
+  [82, '82'],
+  [3, 'Fizz'],
+  [66, 'Fizz'],
+  [99, 'Fizz'],
+  [5, 'Buzz'],
+  [50, 'Buzz'],
+  [85, 'Buzz'],
+  [15, 'FizzBuzz'],
+  [30, 'FizzBuzz'],
+  [45, 'FizzBuzz'],
+]);
